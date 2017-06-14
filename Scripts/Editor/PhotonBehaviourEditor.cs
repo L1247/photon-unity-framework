6ca41e5c9111e0f01b5aa8dc3191c7cf3ce6dfce
L1247 <starlove124760@gmail.com>
2017-06-15 03:04:05 +0800

    增加Editor資料夾及腳本

---
+++ Scripts/Editor/PhotonBehaviourEditor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor( typeof( PhotonBehaviourScript ) )]
+public class PhotonBehaviourEditor : Editor 
+{
+        private SerializedObject m_Object;
+        private SerializedProperty m_stringValue;
+
+       
+        public void OnEnable ()
+        {
+                m_Object = new SerializedObject( target );
+                m_stringValue = m_Object.FindProperty( "pbEnum" );
+                //Debug.Log( m_stringValue.stringValue );
+        }
+
+        public override void OnInspectorGUI ()
+        {
+                DrawDefaultInspector();
+
+                PhotonBehaviourScript myScript = ( PhotonBehaviourScript ) target;
+                if ( GUILayout.Button( "Build Object" ) )
+                {
+                        myScript.BuildType();
+                }
+                if ( GUI.changed )
+                {
+                        Debug.Log( "Changed" );
+                }
+        }
+}
