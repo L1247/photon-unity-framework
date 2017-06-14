42dab5d938da5f1f140141e2d09b8325e23c3a81
Star <starlove124760@gmail.com>
2017-06-14 11:38:07 +0800

    將原本恐龍的Prefab與腳本拉進來

---
+++ Scripts/PhotonPlayerHandler.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class PhotonPlayerHandler : Photon.PunBehaviour
+{
+    public static PhotonPlayerHandler instance;
+
+    void Awake ( )
+    {
+        instance = this;
+        DontDestroyOnLoad( this );
+    }
+
+    public void OnPhotonPlayerConnected ( PhotonPlayer newPlayer )
+    {
+        if ( PhotonNetwork.isMasterClient )
+        {
+            //print( newPlayer.ID );
+            PlayerFinder.PlayerConnected( newPlayer );
+        }
+    }
+
+    public void OnPhotonPlayerDisconnected ( PhotonPlayer otherPlayer )
+    {
+        if ( PhotonNetwork.isMasterClient )
+        {
+            //print( otherPlayer );
+            PlayerFinder.PlayerDisconnected( otherPlayer );
+        }
+    }
+
+    public void SyncPlayerStandSpaceList ( int[ ] standPlaceList )
+    {
+        photonView.RPC( "ReveicePlayerStandData" , PhotonTargets.Others , standPlaceList );
+    }
+
+    [PunRPC]
+    private void ReveicePlayerStandData ( int[ ] standPlaceList )
+    {
+        //foreach ( var item in standPlaceList )
+        //{
+        //    Debug.Log( item );
+        //}
+        PlayerFinder.SetPlayerStandSpaceDic( standPlaceList );
+    }
+
+    /// <summary>Called by Unity when the application is closed. Disconnects.</summary>
+    protected void OnApplicationQuit ( )
+    {
+        VerifyCommon.FlushPlayTimes();
+        PhotonNetwork.Disconnect();
+    }
+
+}
---
+++ Scripts/PhotonPlayerRegister.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 幫助Avatar註冊，方便管理所有需要控制位置的角色初始化.
+/// </summary>
+public class PhotonPlayerRegister : Photon.PunBehaviour
+{
+    private int ownerID;
+
+    void Awake ( )
+    {
+        ownerID = photonView.owner.ID;
+        PlayerFinder.RegPlayer( transform , ownerID );
+    }
+    void OnDestroy ( )
+    {
+        PlayerFinder.UnRegPlayer( ownerID );
+    }
+}
