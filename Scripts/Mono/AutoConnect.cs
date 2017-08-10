7ad1e73eee0e9b28124671f8572a5a1e4db81b46
Star <starlove124760@gmail.com>
2017-08-10 12:31:49 +0800

    增加自動連線、Prefab

---
+++ Scripts/Mono/AutoConnect.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoConnect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        PhotonNetwork.ConnectUsingSettings("DinoGen2.0");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
