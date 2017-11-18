272d8ef6ff608b679a983589fadd0d2b6c2eae95
Star <starlove124760@gmail.com>
2017-11-18 16:05:01 +0800

    增加MonoType

--- Scripts/InheritanceBase/srPhotonBehaviour.cs
+++ Scripts/InheritanceBase/srPhotonBehaviour.cs
@@ -1,13 +1,23 @@
 ﻿using UnityEngine;
 
+
 /// <summary>
 /// 負責設定 Photon連線時，非MasterClient是否開啟此GameObject的屬性等等
 /// </summary>
 public class srPhotonBehaviour : Photon.PunBehaviour
 {
+    public enum MonoType
+    {
+        Mono,
+        IPhoton,
+    }
+
     [Header("Other Client   ")]
     [SerializeField]
     private bool ScriptEnable , GameObjectActive;
+
+    [SerializeField]
+    private MonoType m_MonoType ;
     // Use this for initialization
     protected virtual void Awake ( )
     {
@@ -24,20 +34,36 @@ public class srPhotonBehaviour : Photon.PunBehaviour
 
     void SetMonoProperty ( )
     {
-        iPhoton[] iPhotons = GetComponents<iPhoton>();
-        for ( int i = 0 ; i < iPhotons.Length ; i++ )
+        switch ( m_MonoType )
         {
-            iPhoton _iPhoton = iPhotons[ i ];
-            if ( _iPhoton != null )
-            {
-                if ( PhotonNetwork.isMasterClient == false )
+            case MonoType.Mono:
+                MonoBehaviour[] monos = GetComponents<MonoBehaviour>();
+                srMonoUtility.SetActive(gameObject, GameObjectActive );
+                for ( int i = 0 ; i < monos.Length ; i++ )
                 {
-                    //print( "Not MasterClient" );
-                    MonoBehaviour mono = _iPhoton as MonoBehaviour;
+                    MonoBehaviour mono = monos[ i ];
                     srMonoUtility.SetEnable( mono , ScriptEnable );
-                    srMonoUtility.SetActive( mono.gameObject , GameObjectActive );
                 }
-            }
+                break;
+            case MonoType.IPhoton:
+                iPhoton[] iPhotons = GetComponents<iPhoton>();
+                for ( int i = 0 ; i < iPhotons.Length ; i++ )
+                {
+                    iPhoton _iPhoton = iPhotons[ i ];
+                    if ( _iPhoton != null )
+                    {
+                        if ( PhotonNetwork.isMasterClient == false )
+                        {
+                            //print( "Not MasterClient" );
+                            MonoBehaviour mono = _iPhoton as MonoBehaviour;
+                            srMonoUtility.SetEnable( mono , ScriptEnable );
+                            srMonoUtility.SetActive( mono.gameObject , GameObjectActive );
+                        }
+                    }
+                }
+                break;
+            default:
+                break;
         }
     }
 }
